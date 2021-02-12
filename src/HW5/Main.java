820e513fc19da9a4b8d298581aa0c6258b772da2
BIS84 <bbiiss84@mail.ru>
2021-02-12 18:53:27 +0300

    add task 1

---
+++ src/HW5/Main.java
@@ -0,0 +1,16 @@
+package HW5;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        String[] header1 = {"number", "value", "measurementError"};
+        int[][] data1 = {{1, 102, 1}, {2, 125, 3}, {3, 156, 2}, {4, 256, 1}};
+
+        AppData table = new AppData(header1, data1);
+
+        table.fileWriter(table);
+    }
+}
